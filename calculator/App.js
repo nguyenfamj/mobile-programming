9cb348c5445e149c9b9c974b20f1b5524b5bffe6
nguyenfamj <nguyenfamj1409@gmail.com>
2022-09-04 03:11:07 +0300

    Calculator done

--- calculator/App.js
+++ calculator/App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [calc, setCalc] = useState({ result: 0, nums: { first: 0, second: 0 } });
+
+  const onInputChange = (name, value) => {
+    setCalc({ ...calc, nums: { ...calc.nums, [name]: Number(value) } });
+  };
+
+  const increaseChange = () => {
+    setCalc({ ...calc, result: calc.nums.first + calc.nums.second });
+  };
+
+  const decreaseChange = () => {
+    setCalc({ ...calc, result: calc.nums.first - calc.nums.second });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result: {calc.result}</Text>
+      <View>
+        <TextInput
+          style={{ width: 150, borderColor: 'gray', borderWidth: 1 }}
+          value={calc.nums.first}
+          onChangeText={(text) => onInputChange('first', text)}
+          keyboardType='numeric'
+        />
+        <TextInput
+          style={{ width: 150, borderColor: 'gray', borderWidth: 1 }}
+          value={calc.nums.second}
+          onChangeText={(text) => onInputChange('second', text)}
+          keyboardType='numeric'
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          width: '30%',
+          marginTop: 10,
+        }}
+      >
+        <Button title='+' onPress={increaseChange} />
+        <Button title='-' onPress={decreaseChange} />
+      </View>
     </View>
   );
 }
